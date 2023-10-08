4e6d05a14a41c51c84f25933fd1a3c3d67f977b8
sven <sven@Synth1N>
2023-10-08 18:08:49 +0200

    commit changes

---
+++ Java_CLI_Calculator/src/com/javal/calculator/ApplicationMain.java
@@ -0,0 +1,18 @@
+/*
+ * Console-Based Calculator: Create a console-based calculator program that 
+ * accepts user input for arithmetic operations (addition, subtraction, 
+ * multiplication, division) and displays the results.
+ * 
+ * 
+ * */
+
+package com.javal.calculator;
+
+public class ApplicationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
