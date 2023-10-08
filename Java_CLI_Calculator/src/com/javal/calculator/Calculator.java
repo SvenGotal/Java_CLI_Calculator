0dbeafac2c907686d5b4de3884b5a5d0b99b0324
sven <sven@Synth1N>
2023-10-08 18:31:32 +0200

    Add Calculator constructors

--- Java_CLI_Calculator/src/com/javal/calculator/Calculator.java
+++ Java_CLI_Calculator/src/com/javal/calculator/Calculator.java
@@ -1,5 +1,23 @@
 package com.javal.calculator;
 
 public class Calculator {
-
+	
+	private Double operand1;
+	private Double operand2;
+	private String operator;
+	
+	public Calculator() {
+		this.operand1 = 0.0;
+		this.operand2 = 0.0;
+		this.operator = "+";
+	}
+	
+	public Calculator(Double operand1, String operator, Double operand2) {
+		this.operand1 = operand1;
+		this.operand2 = operand2;
+		this.operator = operator;
+	}
+	
+	
+	
 }
